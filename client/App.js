ffa82fc0194a26548588a6494cb8eed25854474e
RoqueHernez <roquejr14hernandez@gmail.com>
2022-11-23 12:24:56 -0500

    React Components Created &&  Font Downloaded

--- client/App.js
+++ client/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Routes from './Routes'
-
+import UserTranslatePage from "./components/UserTranslatePage/UserTranslatePage";
 const App = () => {
   return (
     <div>
       <Navbar />
+      <UserTranslatePage />
       <Routes />
     </div>
   )
