55e0f363860bb1a0f7e9c8aaa5e63b79ce7874b0
jphwang212 <jphwang212@gmail.com>
2022-11-30 16:46:59 -0500

    Post route connected OutputContainer <=> api/prompts

--- client/Watson.js
+++ client/Watson.js
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import ServiceContainer from "./components/Watson/ServiceContainer";
 
 // var recognizeMic = require('watson-speech/speech-to-text/recognize-microphone');
 
 const Watson = () => {
-  const dispatch = useDispatch();
-  const { transcript } = useSelector((state) => state);
-
   return (
     <div>
       <h2>Placeholder for Watson</h2>
       <ServiceContainer />
-      <div>
-        <button onClick={compare}>Submit for Comparison</button>
-      </div>
     </div>
   );
 };
