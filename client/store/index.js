ae93ad88118430a4040463062d60e7080bb75bdb
jphwang212 <jphwang212@gmail.com>
2022-11-30 12:31:41 -0500

    added watson dummy route

--- client/store/index.js
+++ client/store/index.js
@@ -1,12 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import prompt from './prompt'
-import prompts from './prompts'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import auth from "./auth";
+import prompt from "./prompt";
+import prompts from "./prompts";
+import resp from "./watson";
 
-const reducer = combineReducers({ auth, prompt, prompts })
+const reducer = combineReducers({ auth, prompt, prompts, resp });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
