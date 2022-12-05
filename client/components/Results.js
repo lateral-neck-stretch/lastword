c42804ed954249e36b944bc9cacf34a2a2ec9dd6
Brian Kropaczek <kropaczekb@gmail.com>
2022-12-05 13:09:07 -0500

    Sentence color changes depending on score

--- client/components/Results.js
+++ client/components/Results.js
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, connect } from "react-redux";
 import { setTranscript } from "../store/watson";
-import { postResult } from "../store/result"
+import { postResult } from "../store/result";
+import $ from 'jquery';
 
 function Results(props) {
   /// Probably just get transcript through props
-  // const [transcript, setTranscript] = useState(null);
   const [result, setResult] = useState(null);
   const [vocab, setVocab] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // const usableTranscript = props.location.state.transcript.join(" ");
-
     const func = async () => {
         const { data } = await axios.get("/auth/twinwords");
         let resultsArray = await Promise.all(props.location.state.transcript.map(async (sentence, index) => {
@@ -30,17 +28,11 @@ function Results(props) {
           console.log(response.data)
           return response.data;
         }))
-        // console.log(resultsArray)
         setResult(resultsArray)
-        // const average = (resultsArray.reduce((previous, current) => { return previous + current.similarity}, 0) / resultsArray.length)
-        // console.log(average)
-        // setResult(average)
       }
       func()
       dispatch(setTranscript([]));
     }, []);
-
-    // console.log(resultsArray)
 
     /////////// Grab the actual transcript probably passed as props from translate page component
     // useEffect(() => {
@@ -51,6 +43,7 @@ function Results(props) {
   //   }
   //   getTranscript();
   // }, []);
+
   useEffect(() => {
     let vocabObject = JSON.parse(props.location.state.vocabulary)
     let joinedTranscript = props.location.state.transcript.join("").toLowerCase()
@@ -76,22 +69,43 @@ function Results(props) {
   useEffect(() => {
     if (result && vocab) {
       const token = window.localStorage.getItem("token");
-      let apiScore = Math.floor(result.reduce((previous, current) => { return previous + current.similarity}, 0) / result.length * 100)
+      let apiScore = Math.floor(result.reduce((previous, current) => { return previous + current.similarity}, 0) / result.length * 100 * (result.length / props.location.state.length))
       let vocabScore = Math.floor(vocab.reduce((previous, current) => { return previous + current }, 0) / vocab.length * 100)
       let overallScore = Math.floor((apiScore + vocabScore) / 2)
       props.postResult(overallScore, vocabScore, apiScore, 1, props.location.state.id, token)
     }
   }, [result, vocab])
-  console.log(props.result)
+
+  useEffect(() => {
+    if (result) {
+    console.log(result)
+    result.forEach((apiSentenceScore, index) => {
+      let htmlId = "#sentence_no_" + index;
+      console.log(htmlId)
+      let htmlSentence = $(htmlId)
+      if (apiSentenceScore.similarity < 0.5) {
+        htmlSentence.css("color", "red")
+      } else if (apiSentenceScore.similarity < 0.8) {
+        htmlSentence.css("color", "yellow")
+      } else {
+        htmlSentence.css("color", "green")
+      }
+    })
+  }
+  }, [result])
 
   return (
     <div>
       <h1>Results:</h1>
-      <div>{(result) ? ("Translation score: " + (result.reduce((previous, current) => { return previous + current.similarity}, 0) / result.length) * 100 + "%") : (null)}</div>
+      <div>{(result) ? ("Translation score: " + (result.reduce((previous, current) => { return previous + current.similarity}, 0) / result.length) * 100 * (result.length / props.location.state.transcript.length) + "%") : (null)}</div>
       <div>{(vocab) ? ("Vocabulary score: " + (vocab.reduce((previous, current) => { return previous + current }, 0) / vocab.length) * 100 + "%") : (null)}</div>
-      <p>
-        {props.location.state.transcript}
-      </p>
+        {props.location.state.transcript.map((sentence, index) => {
+          let divId = "sentence_no_" + index
+          console.log(divId)
+          return (
+            <div key={index} id={divId}>{sentence}</div>
+          )
+        })}
     </div>
   );
 }
