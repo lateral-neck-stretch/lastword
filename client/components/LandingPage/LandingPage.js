6556be6f07e4e252fa104448d7efabf609f40e79
Anthony Zhang <zhang.anthony97@gmail.com>
2022-12-08 16:24:35 -0500

    sticky sidebar with magicscroll inside

--- client/components/LandingPage/LandingPage.js
+++ client/components/LandingPage/LandingPage.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-function LandingPage() {
-  return (
-    <div>
-      <h1>Welcome to the Landing Page</h1>
-      <p>This is a study app using Watson.</p>
+// import React from 'react'
+// import { Link } from 'react-router-dom';
+// function LandingPage() {
+//   return (
+//     <div>
+//       <h1>Welcome to the Landing Page</h1>
+//       <p>This is a study app using Watson.</p>
 
-      <Link to="/watson">Checkout Watson</Link>
-    </div>
-  )
-}
+//       <Link to="/watson">Checkout Watson</Link>
+//     </div>
+//   )
+// }
 
-export default LandingPage;
+// export default LandingPage;
--- client/components/UserProfile/UserProfile.js
+++ client/components/UserProfile/UserProfile.js
@@ -5,6 +5,7 @@ import { connect, useSelector, useEffect } from "react-redux";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import style from "./UserProfile.module.css";
+import { Controller, Scene } from "react-scrollmagic";
 
 function UserProfile(props) {
   // const singleUser = useSelector((state) => state.user);
@@ -18,17 +19,47 @@ function UserProfile(props) {
   }, []);
 
   return (
-    <div>
-      {/* <h1>Welcome, {username}</h1> */}
-
+    <div className={style.sidebarPage}>
       {/* ///USER AVATAR/// */}
-      <div class="column" className={style.sideBar}>
+      <div className={style.sideBar} side="right">
         <Avatar
           alt="user_avatar"
           src="/triceratops_avatar.png"
           sx={{ width: 100, height: 100, bgcolor: "white" }}
         />
-        <h3>Hi, {username} </h3>
+        <h2>Hi, {username} </h2>
+        <h4>Level 8</h4>
+        <h4>User since 2022</h4>
+        <h3>Languages:</h3>
+        <ul>
+          {/* hardcoded but can use map function for every user */}
+          <li>English</li>
+          <li>Spanish</li>
+          <li>Portuguese</li>
+        </ul>
+      </div>
+
+      <div className={style.scrollComponent}>
+        <section>
+          <Controller>
+            <Scene
+              duration={800}
+              pin={{ pushFollowers: true }}
+              triggerHook={0.5}
+              offset={125}
+            >
+              <div> Test test pg 1 </div>
+            </Scene>
+            <Scene
+              duration={800}
+              pin={{ pushFollowers: true }}
+              triggerHook={0.5}
+              offset={125}
+            >
+              <div> Test test pg 2 </div>
+            </Scene>
+          </Controller>
+        </section>
       </div>
     </div>
   );
