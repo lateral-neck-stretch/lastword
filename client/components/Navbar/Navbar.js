45dc07fc884f90b837c5f72f2e2b71b4296b77fb
Anthony Zhang <zhang.anthony97@gmail.com>
2022-12-09 15:50:06 -0500

    set up links on navbar

--- client/components/Navbar/Navbar.js
+++ client/components/Navbar/Navbar.js
@@ -1,32 +1,33 @@
-import * as React from 'react';
-import style from './Navbar.module.css';
-import logo from './logo_white.png';
-import cornerLogo from './corner_logo_white.png';
-import { connect } from 'react-redux';
-import { logout } from '../../store';
-import history from '../../history';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import style from "./Navbar.module.css";
+import logo from "./logo_white.png";
+import cornerLogo from "./corner_logo_white.png";
+import { connect } from "react-redux";
+import { logout } from "../../store";
+import history from "../../history";
+import { Link } from "react-router-dom";
 
 /// MUI ///
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
-const pages = ['Home', 'Prompts', 'About'];
-const settings = ['Profile', 'Account', 'Logout'];
+const pages = ["Home", "Prompts", "Leaderboard", "About"];
+const settings = ["Profile", "Account"];
 
-function Navbar() {
+function Navbar(props) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { handleLogout, isLoggedIn } = props;
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -35,7 +36,9 @@ function Navbar() {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
+  const handleCloseNavMenu = (page) => {
+    console.log(page);
+    window.location.href = `/${page}`;
     setAnchorElNav(null);
   };
 
@@ -43,54 +46,76 @@ function Navbar() {
     setAnchorElUser(null);
   };
   return (
-    <AppBar className={style.navbar} position='static'>
-      <Container maxWidth='xl' border-bottom='white'>
+    <AppBar className={style.navbar} position="static">
+      <Container maxWidth="xl" border-bottom="white">
         <Toolbar disableGutters>
-          <img className={style.logo} src={cornerLogo} alt='lastword logo' />
+          <img className={style.logo} src={cornerLogo} alt="lastword logo" />
           <Box
             className={style.navbar_links}
-            sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
           >
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
+              <a href={`/${page}`}>
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              </a>
             ))}
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title='Open settings'>
+            <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt='user avatar' sx={{ bgcolor: 'transparent' }}>
+                <Avatar alt="user avatar" sx={{ bgcolor: "transparent" }}>
                   <AccountCircle style={{ width: 50, height: 50 }} />
                 </Avatar>
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
-              id='menu-appbar'
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign='center'>{setting}</Typography>
-                </MenuItem>
+                <a href={`/${setting}`}>
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                </a>
               ))}
+              {isLoggedIn ? (
+                <MenuItem key={logout} onClick={handleLogout}>
+                  <Typography textAlign="center">Logout</Typography>
+                </MenuItem>
+              ) : (
+                <>
+                  <a href="/login">
+                    <MenuItem key="login" onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">Login</Typography>
+                    </MenuItem>
+                  </a>
+                  <a href="/signup">
+                    <MenuItem key="signup" onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">Sign Up</Typography>
+                    </MenuItem>
+                  </a>
+                </>
+              )}
             </Menu>
           </Box>
         </Toolbar>
@@ -105,6 +130,7 @@ import { connect } from 'react-redux';
 import { logout } from '../../store';
 import history from '../../history';
 import { Link } from 'react-router-dom';
+
 function Navbar(props) {
 const { handleLogout, isLoggedIn } = props;
 return (
@@ -141,8 +167,8 @@ return (
 // }
 const mapNav = (state) => {
   return {
-    name: 'logout',
-    displayName: 'Logout',
+    name: "logout",
+    displayName: "Logout",
     isLoggedIn: !!state.auth.id,
   };
 };
